e486d3932692e6daa00f14338946011f5d8d2c55
CameronJWalker <cameron.jr.walker@icloud.com>
2023-06-17 20:34:23 -0700

    added baseline for navbar

---
+++ components/navbar.js
@@ -0,0 +1,16 @@
+import Nav from '../styles/Nav.module.css'
+import Link from "next/link"
+
+export default function Navbar() {
+    return (
+        <div className={Nav.container}>
+            <Link href="/menu" className={Nav.links}> Menu </Link>
+            <Link href="/">
+                <div className={Nav.logo}>
+                    <img src='logo.png'/>
+                </div>
+            </Link>
+            <Link href="/menu" className={Nav.links}> Contact Us </Link>
+        </div>
+    )
+}
